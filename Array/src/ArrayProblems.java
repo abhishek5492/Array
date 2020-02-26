95c948248363e25ea354002ebf6c12537e64d692
Abhishek kumar <abhishek.kumar1@tivo.com>
2020-02-26 10:11:45 +0530

    Puzzle product array

--- Array/src/ArrayProblems.java
+++ Array/src/ArrayProblems.java
@@ -1,5 +1,12 @@
 
 public class ArrayProblems {
+	
+	public void PrintArray(int [] arr) {
+		int n = arr.length;
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 
 	public void GenerateSubSequence(int [] arr) {
 		
@@ -25,4 +32,22 @@ public class ArrayProblems {
 				}
 			}
 		}
+
+	public void ProductArrayPuzzle(int [] arr) {
+		int n = arr.length;
+		int [] left = new int[n];
+		int [] right = new int[n];		
+		left[0] = 1;
+		for(int i = 1; i< n; i++) {
+			left[i] = left[i-1] * arr[i-1];
+		}
+		right[n-1] = 1;
+		for(int i = n-2; i>=0; i--) {
+			right[i] = right[i+1] * arr[i+1];
+		}
+		for(int i = 0; i < n; i++) {
+			System.out.print(left[i] * right[i] + " ");
+		}
+		
+	}
 }
